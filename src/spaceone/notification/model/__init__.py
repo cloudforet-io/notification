67e07ba98e3488ba9c4d66a7c089fbc39a2460ff
Jihyung Song <bluese@megazone.com>
2021-06-05 22:05:06 +0900

    Add schedule model

--- src/spaceone/notification/model/__init__.py
+++ src/spaceone/notification/model/__init__.py
@@ -2,3 +2,4 @@ from spaceone.notification.model.protocol_model import Protocol
 from spaceone.notification.model.notification_model import Notification
 from spaceone.notification.model.project_channel_model import ProjectChannel
 from spaceone.notification.model.user_channel_model import UserChannel
+from spaceone.notification.model.schedule_model import Schedule
--- src/spaceone/notification/model/user_channel_model.py
+++ src/spaceone/notification/model/user_channel_model.py
@@ -1,11 +1,6 @@
 from mongoengine import *
 from spaceone.core.model.mongo_model import MongoModel
-
-
-class Schedule(EmbeddedDocument):
-    day_of_week = ListField(StringField(choices=['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'], required=True))
-    start_hour = IntField(required=True, default=0)
-    end_hour = IntField(required=True)
+from spaceone.notification.model.schedule_model import Schedule
 
 
 class UserChannel(MongoModel):
