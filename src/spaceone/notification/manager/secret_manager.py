20543c7072660b57674368684a017560bc3b3956
Jihyung Song <bluese@megazone.com>
2021-11-02 16:24:19 +0900

    convert to SpaceConnector

--- src/spaceone/notification/manager/secret_manager.py
+++ src/spaceone/notification/manager/secret_manager.py
@@ -1,10 +1,9 @@
 import logging
 
-from spaceone.core import cache
 from spaceone.core.manager import BaseManager
+from spaceone.core.connector.space_connector import SpaceConnector
 from spaceone.notification.error import *
-from spaceone.notification.connector.secret_connector import SecretConnector
-from pprint import pprint
+
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -13,25 +12,25 @@ class SecretManager(BaseManager):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.secret_connector: SecretConnector = self.locator.get_connector('SecretConnector')
+        self.secret_connector: SpaceConnector = self.locator.get_connector('SpaceConnector', service='secret')
 
     def create_secret(self, params):
-        return self.secret_connector.create_secret(params)
+        return self.secret_connector.dispatch('Secret.create', params)
 
     def update_secret(self, params):
-        return self.secret_connector.update_secret(params)
+        return self.secret_connector.dispatch('Secret.update', params)
 
     def update_secret_data(self, params):
-        return self.secret_connector.update_secret_data(params)
+        return self.secret_connector.dispatch('Secret.update_data', params)
 
     def delete_secret(self, params):
-        self.secret_connector.delete_secret(params)
+        return self.secret_connector.dispatch('Secret.delete', params)
 
     def list_secrets(self, query, domain_id):
-        return self.secret_connector.list_secrets(query, domain_id)
+        return self.secret_connector.dispatch('Secret.list', {'query': query, 'domain_id': domain_id})
 
     def get_secret_data(self, secret_id, domain_id):
-        response = self.secret_connector.get_secret_data(secret_id, domain_id)
+        response = self.secret_connector.dispatch('Secret.get_data', {'secret_id': secret_id, 'domain_id': domain_id})
         return response['data']
 
     def get_plugin_secret_data(self, secret_id, supported_schema, domain_id):
@@ -42,54 +41,6 @@ class SecretManager(BaseManager):
             raise ERROR_NOT_FOUND(key='plugin_info.secret_id', value=secret_id)
 
         return self.get_secret_data(secret_id, domain_id)
-
-    # def get_resource_secret_data(self, resource_id, secret_filter, domain_id):
-    #     secret_query = self._make_query(**secret_filter)
-    #
-    #     response = self.list_secrets(secret_query, domain_id)
-    #
-    #     if response.get('total_count', 0) == 0:
-    #         raise ERROR_RESOURCE_SECRETS_NOT_EXISTS(resource_id=resource_id)
-    #
-    #     result = response['results'][0]
-    #     secret_id = result['secret_id']
-    #     schema = result.get('schema')
-    #
-    #     return self.get_secret_data(secret_id, domain_id), schema
-
-    # def get_plugin_secret(self, plugin_id, secret_id, provider, capability, domain_id):
-    #     use_resource_secret = capability.get('use_resource_secret', False)
-    #     supported_schema = capability.get('supported_schema', [])
-    #
-    #     self._check_plugin_secret(use_resource_secret, secret_id, provider)
-    #
-    #     if use_resource_secret:
-    #         secret_query = self._make_query(supported_schema=supported_schema, provider=provider)
-    #     else:
-    #         secret_query = self._make_query(supported_schema=supported_schema, secret_id=secret_id)
-    #
-    #     response = self.list_secrets(secret_query, domain_id)
-    #
-    #     if response.get('total_count', 0) == 0:
-    #         if use_resource_secret:
-    #             raise ERROR_SUPPORTED_SECRETS_NOT_EXISTS(plugin_id=plugin_id, provider=provider)
-    #         else:
-    #             raise ERROR_NOT_FOUND(key='plugin_info.secret_id', value=secret_id)
-    #
-    #     result = response['results'][0]
-    #     secret_id = result['secret_id']
-    #     schema = result.get('schema')
-    #
-    #     return self.get_secret_data(secret_id, domain_id), schema
-
-    # @staticmethod
-    # def _check_plugin_secret(use_resource_secret, secret_id, provider):
-    #     if use_resource_secret:
-    #         if provider is None:
-    #             raise ERROR_REQUIRED_PARAMETER(key='plugin_info.provider')
-    #     else:
-    #         if secret_id is None:
-    #             raise ERROR_REQUIRED_PARAMETER(key='plugin_info.secret_id')
 
     @staticmethod
     def _make_query(**secret_filter):
