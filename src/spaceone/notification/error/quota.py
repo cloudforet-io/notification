8f0a8eb7d000a47466a131ac3e219fae1cd59298
Jihyung Song <bluese@megazone.com>
2022-05-03 15:53:23 +0900

    feat: update quota error message

--- src/spaceone/notification/error/quota.py
+++ src/spaceone/notification/error/quota.py
@@ -6,4 +6,5 @@ class ERROR_QUOTA_LIMIT_TYPE(ERROR_BASE):
 
 
 class ERROR_QUOTA_IS_EXCEEDED(ERROR_BASE):
-    _message = 'Dispatch a notification is not possible because the quota is exceeded.{protocol_id}, Limit={limit}'
+    _message = 'Dispatch a notification is not possible because the quota is exceeded. ' \
+               '(Protocol={protocol_id}, Usage={usage}, Limit={limit})'
